87901941212a604cd32d8eca0bb4e2f47ce05d65
hugosenari <hugosenari@gmail.com>
2022-03-22 00:36:35 -0300

    chore: re-index ../nimpkgs/p/polymers

--- v0_1_1/flake.nix
+++ v0_1_1/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-polymers-v0_1_1.flake = false;
   inputs.src-polymers-v0_1_1.owner = "rlipsc";
-  inputs.src-polymers-v0_1_1.ref   = "refs/tags/v0.1.1";
+  inputs.src-polymers-v0_1_1.ref   = "v0_1_1";
   inputs.src-polymers-v0_1_1.repo  = "polymers";
   inputs.src-polymers-v0_1_1.type  = "github";
   
